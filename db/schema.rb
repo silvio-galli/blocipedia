7f7ca32e3c22b655ad211891b44b159e3f53b366
silvio-galli <silvio.galli@gmail.com>
2016-03-08 00:42:44 +0100

    User Sign Up 2nd commit

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@
 ActiveRecord::Schema.define(version: 20160307092904) do
 
   create_table "users", force: :cascade do |t|
+    t.string   "user_name",                           null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -24,6 +25,10 @@ ActiveRecord::Schema.define(version: 20160307092904) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
