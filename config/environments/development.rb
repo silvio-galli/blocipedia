8f53bd52530a05ffc39c3d44e6ee28626e04970e
silvio-galli <silvio.galli@gmail.com>
2016-03-08 18:07:54 +0100

    User Sign Up completed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,14 @@ Rails.application.configure do
   # Devise url options definition
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+address:        'smtp.gmail.com',
+port:           '587',
+authentication: :plain,
+domain:         ENV['GMAIL_DOMAIN'],
+user_name:      ENV['GMAIL_USERNAME'],
+password:       ENV['GMAIL_PASSWORD'],
+enable_starttls_auto: true
+}
 end
