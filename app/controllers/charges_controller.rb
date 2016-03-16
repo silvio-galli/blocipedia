7892babbbc462d67fa50f58fa34f48ea5889b4f9
silvio-galli <silvio.galli@gmail.com>
2016-03-16 15:14:07 +0100

    Added upgrade to premium plan

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,35 @@
+class ChargesController < ApplicationController
+  def new
+    @stripe_btn_data = {
+      key: "#{ Rails.configuration.stripe[:publishable_key] }",
+      description: "Premium Membership for #{current_user.email}",
+      amount: Amount.default
+    }
+  end
+
+  def create
+    # Create a Stripe Customer object, for associating with the charge
+    customer = Stripe::Customer.create(
+      email: current_user.email,
+      card: params[:stripeToken]
+    )
+
+    # Here's the charge
+    charge = Stripe::Charge.create(
+      customer: customer.id,
+      amount: Amount.default,
+      description: "Premium Membership for #{current_user.email}",
+      currency: "usd"
+    )
+    if charge["status"] == "succeeded"
+      current_user.update(role: :premium)
+    end
+    flash[:notice] = "Thanks for your premium subscription, #{current_user.email}. Your plan was upgraded to #{current_user.role.capitalize}."
+    redirect_to wikis_path
+  end
+
+  rescue Stripe::CardError => e
+    flash.now[:alert] = e.message
+    redirect_to new_charge_path
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  resources :charges, only: [:new, :create]
+
   get 'welcome/index'
 
   get 'welcome/about'
