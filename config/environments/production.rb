2cfbad9f545048ccc503dedd0b154df4ea8f9d58
silvio-galli <silvio.galli@gmail.com>
2016-05-04 22:17:50 +0200

    Correct action_mailer host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Devise url options definition
-  config.action_mailer.default_url_options = { host: 'safe-citadel-31281.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'floating-ocean-14149.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
