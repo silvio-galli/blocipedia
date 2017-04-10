2057c5b702825179924fa30485ee6b97f5808bd8
silvio-galli <silvio.galli@gmail.com>
2017-04-10 15:40:38 +0200

    Modified seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,7 @@ member = User.create!(
 
 me = User.create!(
   user_name:          "Fake",
-  email:              "fakeuser049@gmail.com",
+  email:              "fakeuser@example.com",
   password:           "password",
   confirmation_token: "nUL2xBpinJyT7Yy8PEJK",
   confirmed_at:       "2016-03-12 17:01:47"
